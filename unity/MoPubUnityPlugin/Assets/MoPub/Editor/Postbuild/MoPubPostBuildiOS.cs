becf93296d3ad5f2c9fc1aa4e0310d44b4730702
Artur Adib <arturadib@gmail.com>
2016-04-13 11:26:38 -0400

    Fix iOS build

--- unity/MoPubUnityPlugin/Assets/MoPub/Editor/Postbuild/MoPubPostBuildiOS.cs
+++ unity/MoPubUnityPlugin/Assets/MoPub/Editor/Postbuild/MoPubPostBuildiOS.cs
@@ -40,6 +40,9 @@
 			"MoPub/libMoPubSDK.a"
 		};
 
+		private static string nativeCodeInUnityPath = "Assets/MoPub/Editor/Support";
+		private static string nativeCodeInXcodePath = "MoPub/NativeCode";
+
 		[PostProcessBuild(100)]
 		public static void OnPostprocessBuild(BuildTarget buildTarget, string buildPath)
 		{
@@ -51,6 +54,7 @@
 				PreparePlist (buildPath, "MoPub");
 
 				RenameMRAIDSource (buildPath);
+				InjectNativeCode (buildPath);
 			}
 		}
 
@@ -66,7 +70,7 @@
 				iOSTargetOSVersion.iOS_6_0
 			};
 			var isOldiOSVersion = oldiOSVersions.Contains (PlayerSettings.iOS.targetOSVersion);
-			
+
 			if (isOldiOSVersion) {
 				Debug.LogWarning ("MoPub requires iOS 7+. Please change the Target iOS Version in Player Settings to iOS 7 or higher.");
 			}
@@ -76,26 +80,61 @@
 		{
 			string projPath = Path.Combine (buildPath, "Unity-iPhone.xcodeproj/project.pbxproj");
 			PBXProject project = new PBXProject ();
-			project.ReadFromString (File.ReadAllText(projPath));		
+			project.ReadFromString (File.ReadAllText(projPath));
 			string target = project.TargetGuidByName ("Unity-iPhone");
-			
-			AddPlatformFrameworksToProject (platformFrameworks, project, target);		
+
+			AddPlatformFrameworksToProject (platformFrameworks, project, target);
 			AddFrameworksToProject (frameworks, buildPath, project, target);
 			AddPlatformLibsToProject (platformLibs, project, target);
 			AddLibsToProject (libs, project, target, buildPath);
 			AddBuildProperty (project, target, "OTHER_LDFLAGS", "-ObjC");
 
-			File.WriteAllText (projPath, project.WriteToString());			
+			File.WriteAllText (projPath, project.WriteToString());
 		}
 
 		private static void RenameMRAIDSource (string buildPath)
 		{
 			// Unity will try to compile anything with the ".js" extension. Since mraid.js is not intended
-			// for Unity, it'd break the build. So we store the file with a fake extension and after the 
+			// for Unity, it'd break the build. So we store the file with a masked extension and after the
 			// build rename it to the correct one.
-			
-			string basePath = Path.Combine (buildPath, "Frameworks/Plugins/iOS/Fabric/MoPub/MRAID.bundle");
-			File.Move(Path.Combine(basePath, "mraid.js.prevent_unity_compilation"), Path.Combine(basePath, "mraid.js"));
+
+			string[] maskedFiles = Directory.GetFiles (buildPath, "*.prevent_unity_compilation", SearchOption.AllDirectories);
+			foreach (string maskedFile in maskedFiles) {
+				string unmaskedFile = maskedFile.Replace (".prevent_unity_compilation", "");
+				File.Move(maskedFile, unmaskedFile);
+			}
+		}
+
+		private static void InjectNativeCode (string buildPath)
+		{
+			string projPath = Path.Combine (buildPath, "Unity-iPhone.xcodeproj/project.pbxproj");
+			string nativeCodeInUnityFullPath = Path.Combine (Directory.GetCurrentDirectory (), nativeCodeInUnityPath);
+			string nativeCodeInXcodeFullPath = Path.Combine (buildPath, nativeCodeInXcodePath);
+
+			PBXProject project = new PBXProject();
+			project.ReadFromString(File.ReadAllText(projPath));
+
+			DirectoryCopy (nativeCodeInUnityFullPath, nativeCodeInXcodeFullPath, true);
+
+			string targetGuid = project.TargetGuidByName ("Unity-iPhone");
+			string[] files = Directory.GetFiles (nativeCodeInXcodeFullPath, "*", SearchOption.AllDirectories);
+			foreach (string fileFullPath in files) {
+				string fileExt = Path.GetExtension (fileFullPath);
+				if (fileExt == ".meta" || fileExt == ".txt") {
+					continue;
+				}
+
+				string fileName = Path.GetFileName (fileFullPath);
+				string fileGuid = project.AddFile (fileFullPath, Path.Combine (nativeCodeInXcodePath, fileName));
+				project.AddFileToBuild (targetGuid, fileGuid);
+			}
+
+			string[] subdirs = Directory.GetDirectories (nativeCodeInXcodeFullPath);
+			foreach (string subdir in subdirs) {
+				project.AddBuildProperty (targetGuid, "HEADER_SEARCH_PATHS", subdir);
+			}
+
+			File.WriteAllText(projPath, project.WriteToString());
 		}
 	}
 }
