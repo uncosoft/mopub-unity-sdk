275c3c7e15f78dc9cc26ca84bb7d404df4ad56fe
nscharfe <nscharfe@twitter.com>
2016-05-16 15:07:45 -0700

    add test scenes to build. modify ios build script to add `-unity` suffix to sdk version number

--- mopub-ios-sdk-unity-build.sh
+++ mopub-ios-sdk-unity-build.sh
@@ -6,8 +6,16 @@ if [[ -e mopub-ios-sdk-unity/bin/libmopub-ios-sdk-unity.a ]]; then
     mv mopub-ios-sdk-unity/bin/libMoPubSDK.a mopub-ios-sdk-unity/bin/libMoPubSDK.a.backup
 fi
 
+# update version number to have unity suffix
+sed -i.bak 's/^\(#define MP_SDK_VERSION\)\(.*\)"/\1\2-unity"/'  mopub-ios-sdk/MoPubSDK/MPConstants.h
+
 xcrun xcodebuild -project mopub-ios-sdk-unity/mopub-ios-sdk-unity.xcodeproj -scheme "MoPub for Unity" -destination generic/platform=iphoneos clean
 xcrun xcodebuild -project mopub-ios-sdk-unity/mopub-ios-sdk-unity.xcodeproj -scheme "MoPub for Unity" -destination generic/platform=iphoneos build
+
+# after build, undo the unity suffix
+cd mopub-ios-sdk
+git checkout MoPubSDK/MPConstants.h
+cd ..
 
 if [[ $? -ne 0 ]]; then
     echo "Building mopub-ios-sdk-unity failed, quitting..."
@@ -16,7 +24,7 @@ fi
 
 # Copy three artifacts into the unity plugin; libMoPubSDK.a - the unchanged mopub ios sdk, libmopub-ios-sdk-unity.a - the unity specific components, and mraid.js.
 # Due to the treatment of .js files as source code in unity, we must change the extension. The extension gets changed back by the ios post build script within
-# the unity plugin. The end result is an xcode project that contains 'mraid.js'. This removes the need to change the hard-coded extension in the ios sdk. 
+# the unity plugin. The end result is an xcode project that contains 'mraid.js'. This removes the need to change the hard-coded extension in the ios sdk.
 
 cp mopub-ios-sdk-unity/bin/libmopub-ios-sdk-unity.a unity/MoPubUnityPlugin/Assets/Plugins/iOS/mopub/libmopub-ios-sdk-unity.a
 cp mopub-ios-sdk-unity/bin/libMoPubSDK.a unity/MoPubUnityPlugin/Assets/Plugins/iOS/mopub/libMoPubSDK.a
