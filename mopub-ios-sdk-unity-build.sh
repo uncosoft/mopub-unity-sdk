849513486ec6655396e9bc66a71c55b41f9f53d2
Kelly Dun <kdun@twitter.com>
2017-04-04 12:54:45 -0700

    Update iOS unity build script to clean and copy over the header files and custom events to the unity plugin assets folder.

--- mopub-ios-sdk-unity-build.sh
+++ mopub-ios-sdk-unity-build.sh
@@ -31,6 +31,36 @@ cp mopub-ios-sdk-unity/bin/libmopub-ios-sdk-unity.a unity/MoPubUnityPlugin/Asset
 cp mopub-ios-sdk-unity/bin/libMoPubSDK.a unity/MoPubUnityPlugin/Assets/Plugins/iOS/mopub/libMoPubSDK.a
 cp mopub-ios-sdk/MoPubSDK/Resources/MRAID.bundle/mraid.js unity/MoPubUnityPlugin/Assets/Plugins/iOS/mopub/MoPub.bundle/MRAID.bundle/mraid.js.prevent_unity_compilation
 
+# Update the iOS resources used when packaging the unity assets.
+
+echo "Cleaning the contents of unity/MoPubUnityPlugin/Assets/MoPub/Editor/Support/MoPubSDK/"
+rm -f unity/MoPubUnityPlugin/Assets/MoPub/Editor/Support/MoPubSDK/*
+
+echo "Copying all .h files from mopub-ios-sdk/MoPubSDK/ to unity/MoPubUnityPlugin/Assets/MoPub/Editor/Support/MoPubSDK/"
+find mopub-ios-sdk/MoPubSDK -name "*.h" -type f -exec cp {} unity/MoPubUnityPlugin/Assets/MoPub/Editor/Support/MoPubSDK \;
+
+# This is a hack until ADF-2658 makes it in.
+rm -rf mopub-ios-sdk/AdNetworkSupport/AdMob
+cp -R mopub-ios-sdk/AdNetworkSupport/GoogleAdMob mopub-ios-sdk/AdNetworkSupport/AdMob
+rm -rf mopub-ios-sdk/AdNetworkSupport/UnityAds
+cp -R mopub-ios-sdk/AdNetworkSupport/Unity mopub-ios-sdk/AdNetworkSupport/UnityAds
+
+# Clean and update the contents of all the supported iOS custom events. This is to ensure that the unity asset packages
+# are built with the most up to date changes.
+SUPPORT_LIBS=( "AdColony" "AdMob" "Chartboost" "Facebook" "Millennial" "UnityAds" "Vungle" )
+for SUPPORT_LIB in "${SUPPORT_LIBS[@]}"
+do
+    IOS_EXPORT_FOLDERS_SUPPORT="unity/MoPubUnityPlugin/Assets/MoPub/Editor/Support/$SUPPORT_LIB"
+    
+    echo "Updating $SUPPORT_LIB Custom Events"
+    rm -rf $IOS_EXPORT_FOLDERS_SUPPORT
+    cp -R mopub-ios-sdk/AdNetworkSupport/$SUPPORT_LIB $IOS_EXPORT_FOLDERS_SUPPORT
+done
+
+# This is a hack until ADF-2658 makes it in.
+rm -rf mopub-ios-sdk/AdNetworkSupport/AdMob
+rm -rf mopub-ios-sdk/AdNetworkSupport/UnityAds
+
 if [[ $? -ne 0 ]]; then
     echo "Failed copying the mopub-ios-sdk-unity artifact into Unity, quitting..."
     exit 1
