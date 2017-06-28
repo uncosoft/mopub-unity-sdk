1d6014fcee30fb1a29a439d1be4e028394a6bfc7
axu <axu@twitter.com>
2017-06-28 15:12:09 -0700

    Facebook RV

---
+++ unity/MoPubUnityPlugin/Assets/MoPub/Editor/Support/Facebook/FacebookRewardedVideoCustomEvent.h
@@ -0,0 +1,13 @@
+//
+//  FacebookRewardedVideoCustomEvent.h
+//
+//  Created by Pooja Shashidhar on 4/12/17.
+//  Copyright © 2017 Pooja Shashidhar. All rights reserved.
+//
+
+
+#import "MPRewardedVideoCustomEvent.h"
+
+@interface FacebookRewardedVideoCustomEvent : MPRewardedVideoCustomEvent
+
+@end
